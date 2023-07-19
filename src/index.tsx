69a0c0617e0b26f71665f86eade62d138167c413
dragin96 <alexxx55509@mail.ru>
2023-07-19 23:30:52 +0300

    add example for many listener

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import Analitic from './Analitic';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  // <React.StrictMode>
-    <App />
-  // </React.StrictMode>
+import {
+    createBrowserRouter,
+    RouterProvider,
+} from "react-router-dom";
+import ManyNetwork from "./ManyNetwork";
+import Blank from "./Blank";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Blank />,
+    },
+    {
+        path: "/analytics",
+        element: <Analitic />,
+    },
+    {
+        path: "listener",
+        element: <ManyNetwork />,
+    },
+]);
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+    <RouterProvider
+        router={router}
+    />
 );
